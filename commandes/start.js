0ad21831eb2181af82d5127523bdb1fe85850f49
Dark Shadow <155859542+dark-shadow-LK@users.noreply.github.com>
2024-07-05 16:32:22 +0530

    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈
    😈😈𝐃𝐚𝐫𝐤 𝐒𝐡𝐚𝐝𝐨𝐰😈😈

--- commandes/start.js
+++ commandes/start.js
@@ -9,14 +9,14 @@ const audiovn = "./start.mp3";
         mimetype: 'audio/mp4',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Bmw-md",
+        fileName: "shadow-md",
 
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
           title: "Hi human, I am on run",
-          body: "BMW BOT",
-          thumbnailUrl: "https://telegra.ph/file/a238340352ed8841782a7.jpg",
+          body: "Dark Shadow BOT",
+          thumbnailUrl: "https://telegra.ph/file/1ece2e0281513c05d20ee.jpg",
           sourceUrl: '',
           mediaType: 1,
           renderLargerThumbnail: true
